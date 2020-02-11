d7fc10821680bb968aeb25261933de47c86055dd
awestendorp <s1083928@student.hsleiden.nl>
2020-02-11 11:41:45 +0100

    most of initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header ("Enemy Stats")]
+    [SerializeField] float health = 100;
+    [SerializeField] int scoreValue = 150;
+
+    [Header ("Enemy Shooting")]
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 1f;
+    [SerializeField] float maxTimeBetweenShots = 5f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float shotSpeed = 5f;
+
+
+    [Header ("Visual and Sound effects")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] AudioClip enemyDeathSFX;
+    [SerializeField] [Range(0,1)]float deathSoundVolume = 0.7f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.7f;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+        shotCounter -= Time.deltaTime;
+    }
+
+    private void CountDownAndShoot()
+    {
+       
+
+        if(shotCounter <= 0f)
+        {
+            EnemyFire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+
+        
+    }
+
+    private void EnemyFire()
+    {
+        GameObject laser = 
+           Instantiate (
+            projectile,
+            transform.position,
+            Quaternion.identity
+            ) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -shotSpeed);
+        AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(other, damageDealer);
+
+    }
+
+    private void ProcessHit(Collider2D other, DamageDealer damageDealer)
+    {
+        health -= damageDealer.getDamage();
+        damageDealer.Hit();
+        if (health < 1)
+        {
+            Die(other);
+        }
+    }
+
+    private void Die(Collider2D other)
+    {
+
+        FindObjectOfType<GameSession>().addToScore(scoreValue);
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(enemyDeathSFX, Camera.main.transform.position, deathSoundVolume);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("Player")]
+    [SerializeField] float movespeed = 12f;
+    [SerializeField] int health = 200;
+    [SerializeField] GameObject hitbox;
+    [SerializeField] [Range(0, 1)] float deathSoundVolume = 0.7f;
+    [SerializeField] AudioClip DeathSFX;
+
+
+
+    [Header("Projectiles")]
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float shotSpeed = 10f;
+    [SerializeField] float projectileFiringPeriods = 0.1f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.7f;
+
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    Coroutine firingCoroutine;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        HandleSlowMode();
+        Move();
+        fire();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer)
+        {
+            return;
+        }
+        ProcessHit(other, damageDealer);
+        
+
+    }
+
+    private void ProcessHit(Collider2D other, DamageDealer damageDealer)
+    {
+        health -= damageDealer.getDamage();
+        damageDealer.Hit();
+        Destroy(other.gameObject);
+        if (health < 1)
+        {
+            Die();            
+        }
+    }
+
+
+    private void Die()
+    {
+        FindObjectOfType<Level>().LoadGameOver();
+        Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(DeathSFX, Camera.main.transform.position, deathSoundVolume);
+
+    }
+
+            private void fire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            
+            firingCoroutine = StartCoroutine(constFire());
+            
+        }
+        if (Input.GetButtonUp("Fire1"))
+            StopCoroutine(firingCoroutine);
+        {
+            
+        }
+    }
+
+
+    IEnumerator constFire()
+    {
+        while (true)
+        {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        if(movespeed == 4f)
+            {
+                laser.transform.localScale += new Vector3(2f, 0, 0);
+                //change damage
+            }
+            else
+            {
+                //possibly change damage back
+            }
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, shotSpeed);
+            AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
+        yield return new WaitForSeconds(projectileFiringPeriods);
+        }
+    }
+
+    private void HandleSlowMode()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movespeed = 4f;
+            hitbox.transform.localScale = new Vector3(.3f, .3f, .3f);
+        }
+        else
+        {
+            movespeed = 12f;
+            hitbox.transform.localScale = new Vector3(0f, 0f, 0f);
+        }
+    }
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
+        var newxpos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newypos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newxpos, newypos);
+    }
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0.01f, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(0.99f, 0, 0)).x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0.01f, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 0.99f, 0)).y;
+    }
+
+}
